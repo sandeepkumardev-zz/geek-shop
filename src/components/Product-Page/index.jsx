820fe285ba9e3d0879ffb56455cb64005033f0a4
Sandeep <sandeepypb@gmail.com>
2022-04-02 18:46:50 +0530

    feat: 🎸 added product listing feature

--- src/components/Product-Page/index.jsx
+++ src/components/Product-Page/index.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./product-page.scss";
+import ProductCard from "./ProductCard";
 
 function ProductPage() {
-  return <div className="GuiContainer">Product Page</div>;
+  const [products, setProducts] = React.useState(null);
+
+  //   console.log(products);
+
+  React.useEffect(() => {
+    fetch("./MOCK_DATA.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+  return (
+    <div className="GsContainer">
+      <div className="GsProductPage">
+        <div id="GsFilters" className="GsProductPage__filters">
+          Filters
+        </div>
+        <div id="GsProducts" className="GsProductPage__products">
+          <div className="GsProducts__filtersList">Filter List</div>
+          <div className="GsProducts__productsList">
+            {products?.map((product) => (
+              <ProductCard product={product} key={product.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
